e9962e8d5fbab97c69b463bba39ffa71125aaffb
Dan Gaylord <danielgaylord@gmail.com>
2022-01-20 15:41:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, send_from_directory, request, jsonify,make_response
 from flask_cors import CORS, cross_origin
-import boto3
 import backend.piechart as piechart
 import os
 
